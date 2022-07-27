e01497c3fd50a426d9a7489c0e4ae3f981525dba
HARSH DAYAL <95514087+DAYAL-HARSH@users.noreply.github.com>
2022-07-27 23:07:36 +0530

    Create Combinations.cpp

---
+++ Combinations.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    void Helper(vector<vector<int>>&result,vector<int> current, int start, int n, int k){
+        if(current.size() == k){
+            result.push_back(current);
+            return;
+        }
+        for(int i = start; i<=n;i++){
+            current.push_back(i);
+            Helper(result,current,i+1,n,k);
+            current.pop_back();
+        }
+    }
+    vector<vector<int>> combine(int n, int k) {
+        vector<vector<int>> result;
+        vector<int> current;
+        Helper(result,current, 1, n, k);
+        return result;
+    }
+};
